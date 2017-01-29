13e7e80bceaec1583e3319a438ec86b6de438e65
ankamneha <S525698@mail.nwmissouri.edu>
2017-01-28 22:34:26 -0600

    week2 commit02: facultyController.js implemented completely

--- web/controllers/facultyController.js
+++ web/controllers/facultyController.js
@@ -3,5 +3,12 @@
  */
 app.controller("facultyController", ['$scope', '$cookies', '$state', '$http', 'url', '$uibModal', function ($scope, $cookies, $state, $http, url, $uibModal) {
 //Get course data from database
+    $scope.courseData = [];
+    $http.get(url + "/coursesByFaculty/"+ $cookies.get('username')).then(function successCallback(response) {
 
+        $scope.courseData = response.data.info;
+        console.log(JSON.stringify(response.data.info));
+        console.log(JSON.stringify($scope.courseData));
+    }, function errorCallback(response) {
+    })
 }]);
