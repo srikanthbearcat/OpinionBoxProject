c8dd41b68b650622ebdde6c3b1093b3dae67bbbe
[name] <[email address]>
2017-01-28 22:54:13 -0600

    week 03:Commit 01 studentController.js added

---
+++ web/controllers/studentController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by S525796 on 05-01-2017.
+ */
+app.controller("studentController", ['$scope', '$cookies', '$state', '$http', 'url', '$uibModal', function ($scope, $cookies, $state, $http, url, $uibModal) {
+
+}]);
