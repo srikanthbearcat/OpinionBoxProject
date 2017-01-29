d4c7e4ecc1864243d9ee9676dbcc860365690c96
Vinod Kumar Reddy Peram <S525754@mail.nwmissouri.edu>
2017-01-28 23:53:54 -0600

    Weak 02 commit 02 added studentController.php

---
+++ services/controllers/studentController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+$loginStudent = function () use ($app) {
+    try {
+        $postData = $app->request->post();
+        $user_name = $postData['username'];
+        $password = $postData['password'];
+        $core = Core::getInstance();
+        $sql = "SELECT first_name,last_name,user_name,email FROM user_account WHERE user_name=:user_name AND password =:password";
+        $stmt = $core->dbh->prepare($sql);
+        $stmt->bindParam("user_name", $user_name);
+        $stmt->bindParam("password", $password);
+        $response = new stdClass();
+        $response->user_type = "student";
+       
+        echo json_encode($response);
+    } catch (Exception $ex) {
+
+        $app->response()->status(400);
+        $app->response()->header('X-Status-Reason', $ex->getMessage());
+    }
+};
+
+
+//For the url http://localhost/OpinionBox/services/index.php/student/login
+$app->post('/student/login', $loginStudent);
+
+?>
