cb5c97bed31ba7b4cf7fe7421e2c83a3244fbee2
[name] <[email address]>
2017-01-28 22:48:04 -0600

    Week 02:Commit 02 navigationController.js implemented totally

--- web/controllers/navigationController.js
+++ web/controllers/navigationController.js
@@ -2,5 +2,12 @@ app.controller("navigationController", ['$scope','$cookies','$state' ,function (
     //
     $scope.first_name = $cookies.get('first_name');
     $scope.last_name = $cookies.get('last_name');
-   
+    $scope.logout = function () {
+        $cookies.remove('email');
+        $cookies.remove('first_name');
+        $cookies.remove('last_name');
+        $cookies.remove('username');
+        $cookies.remove('user_type');
+        $state.go('login');
+    }
 }]);
