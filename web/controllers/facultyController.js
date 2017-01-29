810b2fa20de8bad1abdd1a1de8760b6134e82573
ankamneha <S525698@mail.nwmissouri.edu>
2017-01-28 22:33:14 -0600

    week2 commit01: facultyController.js file added

---
+++ web/controllers/facultyController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by S525796 on 04-01-2017.
+ */
+app.controller("facultyController", ['$scope', '$cookies', '$state', '$http', 'url', '$uibModal', function ($scope, $cookies, $state, $http, url, $uibModal) {
+//Get course data from database
+
+}]);
