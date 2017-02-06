4c2dc2c0404fb85aff85ddc43c62854207f1aaa6
ankamneha <S525698@mail.nwmissouri.edu>
2017-02-05 21:58:03 -0600

    week4 commit01: added code in settingsController.js

---
+++ web/controllers/settingsController.js
@@ -0,0 +1,11 @@
+app.controller("settingsController", ['$scope', '$cookies', '$state', '$http', 'url', '$uibModal', function ($scope, $cookies, $state, $http, url, $uibModal) {
+//Get course of student data from database
+//     $scope.courseData = [];
+//     $http.get(url + "/coursesByFaculty/"+ $cookies.get('username')).then(function successCallback(response) {
+//
+//         $scope.courseData = response.data.info;
+//         console.log(JSON.stringify(response.data.info));
+//         console.log(JSON.stringify($scope.courseData));
+//     }, function errorCallback(response) {
+//     })
+}]);
