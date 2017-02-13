40d3bfd2b5ed8f777d46babd380eb5bc28170272
srikanthbearcat <s525796@mail.nwmissouri.edu>
2017-02-12 22:58:08 -0600

    add student Homepage - studentController.js

--- web/controllers/studentController.js
+++ web/controllers/studentController.js
@@ -3,12 +3,19 @@
  */
 app.controller("studentController", ['$scope', '$cookies', '$state', '$http', 'url', '$uibModal', function ($scope, $cookies, $state, $http, url, $uibModal) {
 //Get course of student data from database
-//     $scope.courseData = [];
-//     $http.get(url + "/coursesByFaculty/"+ $cookies.get('username')).then(function successCallback(response) {
-//
-//         $scope.courseData = response.data.info;
-//         console.log(JSON.stringify(response.data.info));
-//         console.log(JSON.stringify($scope.courseData));
-//     }, function errorCallback(response) {
-//     })
+    $scope.courseData = [];
+    $http.post(url + "/coursesByStudent/" + $cookies.get('username')).then(function successCallback(response) {
+        console.log(response);
+        // angular.forEach(response.data.info, function () {1
+        // });
+        // $scope.facultyData = response.data.info;
+        $.each(response.data.info, function (i, data) {
+            data.i = i;
+            data.original_course_crn = data.course_crn;
+            $scope.courseData.push(data);
+        });
+        console.log(response);
+        //console.log(JSON.stringify($scope.courseData));
+    }, function errorCallback(response) {
+    })
 }]);
